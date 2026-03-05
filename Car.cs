6579ad614bbf952859a43340bcf206fb92876725
Aa2ra <aatu.rajala@seamk.fi>
2026-03-05 13:15:59 +0200

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,15 @@
+﻿public class Car : Vehicle
+{
+    public string Brand { get; set; }
+
+    public Car(string brand, string model, int horsePower, int weight)
+        : base(model, horsePower, weight)
+    {
+        Brand = brand;
+    }
+
+    public override string GetInfo()
+    {
+        return $"{Brand} {Model} - {HorsePower}hp";
+    }
+}
